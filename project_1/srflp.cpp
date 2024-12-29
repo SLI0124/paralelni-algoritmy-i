d04c8aed2133697261b9610ff53aa989e9968fb0
Vojtech Sliva <sli0124@vsb.cz>
2024-12-29 02:10:00 +0100

    feat(proj1): calculating cost for one permutation

--- project_1/srflp.cpp
+++ project_1/srflp.cpp
@@ -3,7 +3,14 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <thread>
 
+
+/**
+ * Load a file and return its content as a vector of strings
+ * @param filename Name of the file to load
+ * @return A vector of strings containing the lines of the file
+ */
 std::vector<std::string> load_file(const std::string &filename) {
     std::vector<std::string> data;
     std::ifstream file(filename);
@@ -22,6 +29,48 @@ std::vector<std::string> load_file(const std::string &filename) {
     return data;
 }
 
+
+/**
+ * Calculate the cost of a permutation between the faculties based on the given matrix and sizes
+ * @param permutation The permutation of the faculties
+ * @param weights_matrix The matrix containing the weights of the edges between the faculties
+ * @param sizes The sizes of the faculties
+ * @return The cost of the permutation - unsigned long long to avoid overflow and mostly .clang-tidy warnings
+ */
+unsigned long long calculate_cost(const std::vector<int> &permutation,
+                                  const std::vector<std::vector<int> > &weights_matrix,
+                                  const std::vector<int> &sizes) {
+    unsigned long long cost = 0;
+
+    // Calculate the cost of the permutation based on the given matrix and sizes - calculation is in project description
+    for (size_t i = 0; i < permutation.size(); i++) {
+        for (size_t j = i + 1; j < permutation.size(); j++) {
+            const int faculty_1 = permutation[i];
+            const int faculty_2 = permutation[j];
+            // Get the weight of the edge between the two faculties, we want to use the upper triangular matrix to avoid
+            // the zero values since we only get one side of the matrix
+            const unsigned long long weight = (faculty_1 < faculty_2)
+                                                  ? weights_matrix[faculty_1][faculty_2]
+                                                  : weights_matrix[faculty_2][faculty_1];
+
+            // function to calculate distance d of two faculties in permutation formula
+            int distance = (sizes[faculty_1] + sizes[faculty_2]) / 2; // fractional part of the formula
+            for (unsigned long long k = std::min(i, j) + 1; k < std::max(i, j); ++k) {
+                distance += sizes[permutation[k]]; // sum part of the formula
+            }
+            // function SRFLP of permutation formula - sum of all (c * d) values
+            cost += weight * distance;
+        }
+    }
+    return cost; // return the calculated cost of the permutation
+}
+
+
+void branch_and_bound() {
+    // TODO: Finally understand what the hell is required here and what to do and parallelize it
+}
+
+
 int main() {
     const std::string filename = "../project_1/Y-10_t.txt";
     std::vector<std::string> data = load_file(filename);
@@ -32,34 +81,64 @@ int main() {
 
     int number_of_rows = std::stoi(data[0]);
 
-    std::vector<int> sizes;
+    std::vector<int> faculties_sizes;
     std::istringstream sizes_stream(data[1]);
     int size;
     while (sizes_stream >> size) {
-        sizes.push_back(size);
+        faculties_sizes.push_back(size);
     }
 
-    std::vector<std::vector<int> > matrix(number_of_rows, std::vector<int>(number_of_rows, 0));
+    std::vector<std::vector<int> > weights_matrix(number_of_rows, std::vector<int>(number_of_rows, 0));
     for (int i = 0; i < number_of_rows; ++i) {
         std::istringstream row_stream(data[i + 2]);
         for (int j = 0; j < number_of_rows; ++j) {
-            row_stream >> matrix[i][j];
+            row_stream >> weights_matrix[i][j];
         }
     }
 
     std::cout << "Number of rows and columns: " << number_of_rows << std::endl;
     std::cout << "Sizes: ";
-    for (const auto &s: sizes) {
+    for (const auto &s: faculties_sizes) {
         std::cout << s << " ";
     }
     std::cout << std::endl;
 
     std::cout << "Matrix:" << std::endl;
-    for (const auto &row: matrix) {
+    for (const auto &row: weights_matrix) {
         for (const auto &elem: row) {
             std::cout << elem << " ";
         }
         std::cout << std::endl;
+    }
+
+    unsigned int worker_count = std::thread::hardware_concurrency();
+    std::cout << "Number of threads: " << worker_count << std::endl;
+
+    unsigned int total_permutations = 1;
+    for (unsigned int i = 1; i <= number_of_rows; ++i) {
+        total_permutations *= i;
+    }
+    std::cout << "Total permutations: " << total_permutations << std::endl;
+    unsigned int chunk_size = total_permutations / worker_count;
+    std::cout << "Chunk size: " << chunk_size << std::endl;
+
+    // Test the cost calculation
+    {
+        std::vector<int> test_permutation = {6, 5, 4, 3, 2, 1, 0, 7, 8, 9};
+        unsigned long long test_cost = calculate_cost(test_permutation, weights_matrix, faculties_sizes);
+        std::cout << "Test cost: " << test_cost << std::endl;
+
+        std::vector<int> allegedly_best_permutation = {0, 4, 1, 9, 6, 3, 7, 2, 5, 8};
+        unsigned long long allegedly_best_cost =
+                calculate_cost(allegedly_best_permutation, weights_matrix, faculties_sizes);
+        std::cout << "Allegedly best cost: " << allegedly_best_cost << std::endl;
+
+        // revert the order of the faculties - they should gave the same cost
+        std::vector<int> allegedly_best_permutation_reversed = allegedly_best_permutation;
+        std::reverse(allegedly_best_permutation_reversed.begin(), allegedly_best_permutation_reversed.end());
+        unsigned long long allegedly_best_cost_reversed =
+                calculate_cost(allegedly_best_permutation_reversed, weights_matrix, faculties_sizes);
+        std::cout << "Allegedly best cost reversed: " << allegedly_best_cost_reversed << std::endl;
     }
 
     return 0;
