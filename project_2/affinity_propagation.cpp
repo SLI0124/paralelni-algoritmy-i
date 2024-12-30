332e32ff8b762c50b163a764f0f98693ee3841e5
Vojtech Sliva <sli0124@vsb.cz>
2024-12-30 22:52:16 +0100

    feat(proj2): removed big MNIST train dataset & kept the smaller test one

--- project_2/affinity_propagation.cpp
+++ project_2/affinity_propagation.cpp
@@ -5,54 +5,6 @@
 #include <vector>
 #include <algorithm>
 
-std::vector<std::string> merge_two_csv(const std::string &filename1, const std::string &filename2) {
-    std::vector<std::string> data_file1;
-    std::vector<std::string> data_file2;
-    std::vector<std::string> result;
-
-    std::ifstream file1(filename1);
-    if (!file1.is_open()) {
-        std::cerr << "Error: Could not open the file " << filename1 << std::endl;
-        return data_file1;
-    }
-
-    std::ifstream file2(filename2);
-    if (!file2.is_open()) {
-        std::cerr << "Error: Could not open the file " << filename2 << std::endl;
-        return data_file2;
-    }
-
-    std::string line;
-
-    // skip the header from file1
-    std::getline(file1, line);
-
-    // Read the rest of file1
-    while (std::getline(file1, line)) {
-        data_file1.push_back(line);
-    }
-    file1.close();
-
-    // Read the header from file2 (skip it)
-    std::getline(file2, line);
-
-    // Read the rest of file2
-    while (std::getline(file2, line)) {
-        data_file2.push_back(line);
-    }
-    file2.close();
-
-    // Append the contents of both files
-    result.insert(result.end(), data_file1.begin(), data_file1.end());
-    result.insert(result.end(), data_file2.begin(), data_file2.end());
-
-    std::cout << "Size of file1: " << data_file1.size() << std::endl;
-    std::cout << "Size of file2: " << data_file2.size() << std::endl;
-    std::cout << "Size of merged file: " << result.size() << std::endl;
-
-    return result;
-}
-
 std::vector<std::string> read_csv_file(const std::string &filename) {
     std::vector<std::string> data;
     std::ifstream file(filename);
