4c2295cd89af3027f058d9018e526a27fe0b9fe4
Vojtěch Slíva <sli0124@vsb.cz>
2024-12-31 19:17:25 +0100

    docs(proj2): added some explanation comments and docstrings

--- project_2/affinity_propagation.cpp
+++ project_2/affinity_propagation.cpp
@@ -6,6 +6,13 @@
 #include <algorithm>
 #include <limits>
 
+/**
+ * Reads a CSV file and returns its content as a vector of strings.
+ * Each string represents a line in the CSV file.
+ *
+ * @param filename The name of the CSV file to read.
+ * @return A vector of strings containing the lines of the CSV file.
+ */
 std::vector<std::string> read_csv_file(const std::string &filename) {
     std::vector<std::string> data;
     std::ifstream file(filename);
@@ -27,7 +34,15 @@ std::vector<std::string> read_csv_file(const std::string &filename) {
     return data;
 }
 
-std::vector<std::vector<double> > tokenize_csv(const std::vector<std::string> &data, const char delimiter = ',') {
+/**
+ * Tokenizes the lines of a CSV file into a 2D vector of doubles.
+ *
+ * @param data A vector of strings where each string is a line from the CSV file.
+ * @param delimiter The character used to separate values in the CSV file.
+ * @return A 2D vector of doubles representing the tokenized CSV data.
+ */
+std::vector<std::vector<double> > tokenize_csv(const std::vector<std::string> &data,
+                                               const char delimiter = ',') {
     std::vector<std::vector<double> > result;
     for (const std::string &line: data) {
         std::vector<double> row;
@@ -41,6 +56,12 @@ std::vector<std::vector<double> > tokenize_csv(const std::vector<std::string> &d
     return result;
 }
 
+/**
+ * Prints a 2D matrix to the console.
+ *
+ * @param matrix The 2D vector of doubles to print.
+ * @param name The name of the matrix to print.
+ */
 void print_matrix(const std::vector<std::vector<double> > &matrix, const std::string &name) {
     std::cout << name << std::endl;
 
@@ -61,34 +82,41 @@ void print_matrix(const std::vector<std::vector<double> > &matrix, const std::st
 // forward declaration for better code readability
 void create_clusters(const std::vector<std::vector<double> > &matrix_C);
 
+/**
+ * Calculates the similarity matrix for the given data.
+ *
+ * @param data A 2D vector of doubles representing the input data.
+ * @param verbose A boolean flag to indicate whether to print the similarity matrix.
+ * @return A 2D vector of doubles representing the similarity matrix.
+ */
 std::vector<std::vector<double> > calculate_similarity_matrix(const std::vector<std::vector<double> > &data,
                                                               const bool verbose) {
-    const size_t size_n = data.size();
-    const std::vector<double> row(size_n, 0);
-    std::vector<std::vector<double> > similarity_matrix(size_n, row);
+    const size_t size_n = data.size(); // dimension of the data, meaning first dimension of the matrix
+    const std::vector<double> row(size_n, 0); // initialize a row with zeros, meaning second dimension of the matrix
+    std::vector<std::vector<double> > similarity_matrix(size_n, row); // initialize a matrix with zeros
 
+    // initialize the minimal similarity to 0, it helps us to set the diagonal of the similarity matrix
     double minimal_similarity = 0;
 #pragma omp parallel for collapse(2) default(none) shared(data, similarity_matrix, size_n) reduction(min: minimal_similarity)
     for (size_t i = 0; i < size_n; i++) {
         for (size_t j = 0; j < size_n; j++) {
             double similarity = 0;
-
+            // calculate the similarity between data[i] and data[j]
             for (size_t k = 0; k < data[i].size(); k++) {
                 const double difference = data[i][k] - data[j][k];
                 similarity += difference * difference;
             }
+            // formula part for the similarity
             similarity = -similarity;
             minimal_similarity = std::min(minimal_similarity, similarity);
             similarity_matrix[i][j] = similarity;
         }
     }
-
     // set diagonal to mean of minimal similarity
     for (size_t i = 0; i < size_n; i++) {
         similarity_matrix[i][i] = minimal_similarity;
     }
 
-    // print similarity matrix
     if (verbose) {
         print_matrix(similarity_matrix, "Similarity matrix");
     }
@@ -97,17 +125,26 @@ std::vector<std::vector<double> > calculate_similarity_matrix(const std::vector<
     return similarity_matrix;
 }
 
+/**
+ * Performs affinity propagation clustering on the given similarity matrix.
+ *
+ * @param matrix_S A 2D vector of doubles representing the similarity matrix.
+ * @param max_iteration The maximum number of iterations to perform.
+ * @param verbose A boolean flag to indicate whether to print intermediate matrices.
+ * @return A 2D vector of doubles representing the final combined matrix.
+ */
 std::vector<std::vector<double> > calculate_affinity_propagation(const std::vector<std::vector<double> > &matrix_S,
                                                                  const int max_iteration,
                                                                  const bool verbose = false) {
-    const size_t size_n = matrix_S.size();
-    const std::vector<double> row(size_n, 0);
+    const size_t size_n = matrix_S.size(); // same as before, first dimension of the matrix
+    const std::vector<double> row(size_n, 0); // same as before, second dimension of the matrix
     auto matrix_A = std::vector<std::vector<double> >(size_n, row);
     auto matrix_R = std::vector<std::vector<double> >(size_n, row);
     auto matrix_C = std::vector<std::vector<double> >(size_n, row);
 
-    bool changed = true;
-    int iteration = 0;
+    // here we will keep track of variables that are responsible for last change and stopping condition
+    bool changed = true; // flag to indicate if the matrix has changed or not
+    int iteration = 0; // iteration counter
 
     while (changed && iteration < max_iteration) {
         std::cout << "Iteration " << iteration << " out of " << max_iteration << std::endl;
@@ -176,11 +213,10 @@ std::vector<std::vector<double> > calculate_affinity_propagation(const std::vect
             for (size_t k = 0; k < size_n; k++) {
                 const double new_value = matrix_A[i][k] + matrix_R[i][k];
                 const double old_value = matrix_C[i][k];
-
+                // Check if the value has changed to start next iteration
                 if (new_value != old_value) {
                     changed = true;
                 }
-
                 matrix_C[i][k] = new_value;
             }
         }
@@ -201,10 +237,15 @@ std::vector<std::vector<double> > calculate_affinity_propagation(const std::vect
         print_matrix(matrix_C, "Final Combined Matrix");
     }
 
-    std::cout << "Converged after " << iteration << " iterations" << std::endl;
+    std::cout << "Converged after " << iteration << " iterations" << std::endl << std::endl;
     return matrix_C;
 }
 
+/**
+ * Creates clusters based on the combined matrix C.
+ *
+ * @param matrix_C A 2D vector of doubles representing the combined matrix.
+ */
 void create_clusters(const std::vector<std::vector<double> > &matrix_C) {
     const size_t num_rows = matrix_C.size();
     const size_t num_cols = matrix_C[0].size();
@@ -219,8 +260,9 @@ void create_clusters(const std::vector<std::vector<double> > &matrix_C) {
 
         for (size_t j = 0; j < num_cols; j++) {
             if (matrix_C[i][j] > max_value) {
-                max_value = matrix_C[i][j];
-                max_index = static_cast<int>(j);
+                // find the maximum value in the row
+                max_value = matrix_C[i][j]; // update the maximum value
+                max_index = static_cast<int>(j); // update the index of the maximum value
             }
         }
 
@@ -235,7 +277,7 @@ void create_clusters(const std::vector<std::vector<double> > &matrix_C) {
         std::cout << "Cluster " << cluster << ": ";
         for (size_t i = 0; i < num_rows; i++) {
             if (static_cast<size_t>(cluster_assignments[i]) == cluster) {
-                // Cast to size_t
+                // Cast to size_t for .clang-tidy
                 std::cout << i << " ";
             }
         }
@@ -259,7 +301,7 @@ int main() {
         five_participants_similarity_matrix, max_iteration, verbose);
     create_clusters(five_participants_clusters);
 
-    // MNIST test dataset, it's way too big so it takes ages (it did not finish on my machine), uncomment on your own risk
+    // // MNIST test dataset, it's way too big so it takes ages (it did not finish on my machine), uncomment on your own risk
     // verbose = false;
     // const std::string mnist_file_test = "../project_2/mnist_test.csv";
     // const std::vector<std::string> mnist_test_dataset = read_csv_file(mnist_file_test);
@@ -269,7 +311,6 @@ int main() {
     // const std::vector<std::vector<double> > mnist_test_clusters = calculate_affinity_propagation(
     //     mnist_test_similarity_matrix, max_iteration, verbose);
     // create_clusters(mnist_test_clusters);
-
 
     return 0;
 }
