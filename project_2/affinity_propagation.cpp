0e851bfc34c3c934e8c22414eba2a531b0feec3f
Vojtěch Slíva <sli0124@vsb.cz>
2024-12-31 01:13:32 +0100

    feat(proj1): fixed some mumbo jumbo from Linux Cmake errors

--- project_2/affinity_propagation.cpp
+++ project_2/affinity_propagation.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <limits>
 
 std::vector<std::string> read_csv_file(const std::string &filename) {
     std::vector<std::string> data;
@@ -65,11 +66,11 @@ std::vector<std::vector<double> > calculate_similarity_matrix(const std::vector<
 
     double minimal_similarity = 0;
 #pragma omp parallel for default(none) shared(data, similarity_matrix, size_n, minimal_similarity)
-    for (int i = 0; i < size_n; i++) {
-        for (int j = 0; j < size_n; j++) {
+    for (size_t i = 0; i < size_n; i++) {
+        for (size_t j = 0; j < size_n; j++) {
             double similarity = 0;
 
-            for (int k = 0; k < data[i].size(); k++) {
+            for (size_t k = 0; k < data[i].size(); k++) {
                 const double difference = data[i][k] - data[j][k];
                 similarity += difference * difference;
             }
@@ -80,7 +81,7 @@ std::vector<std::vector<double> > calculate_similarity_matrix(const std::vector<
     }
 
     // set diagonal to mean of minimal similarity
-    for (int i = 0; i < size_n; i++) {
+    for (size_t i = 0; i < size_n; i++) {
         similarity_matrix[i][i] = minimal_similarity;
     }
 
@@ -99,21 +100,21 @@ std::vector<std::vector<double> > calculate_affinity_propagation(const std::vect
     auto matrix_C = std::vector<std::vector<double> >(size_n, row);
 
     bool changed = true;
-    long iteration = 0;
+    double iteration = 0;
 
     while (changed && iteration < max_iteration) {
         iteration++;
 
         // Calculate responsibility matrix
 #pragma omp parallel for collapse(2) default(none) shared(matrix_A, matrix_S, matrix_R, size_n)
-        for (int i = 0; i < size_n; i++) {
-            for (int k = 0; k < size_n; k++) {
+        for (size_t i = 0; i < size_n; i++) {
+            for (size_t k = 0; k < size_n; k++) {
                 // S(i,k) part -> initialize R(i,k) with S(i,k)
                 matrix_R[i][k] = matrix_S[i][k];
 
                 // minus part
                 double maximum = std::numeric_limits<double>::min();
-                for (int k_ = 0; k_ < size_n; k_++) {
+                for (size_t k_ = 0; k_ < size_n; k_++) {
                     if (k_ != k) {
                         // max part of the equation -> max(A(i,k') + S(i,k'))
                         maximum = std::max(maximum, matrix_A[i][k_] + matrix_S[i][k_]);
@@ -126,10 +127,10 @@ std::vector<std::vector<double> > calculate_affinity_propagation(const std::vect
 
         // Calculate availability matrix
 #pragma omp parallel for collapse(2) default(none) shared(matrix_A, matrix_R, size_n)
-        for (int i = 0; i < size_n; i++) {
-            for (int k = 0; k < size_n; k++) {
+        for (size_t i = 0; i < size_n; i++) {
+            for (size_t k = 0; k < size_n; k++) {
                 double sum_on_max = 0;
-                for (int i_ = 0; i_ < size_n; i_++) {
+                for (size_t i_ = 0; i_ < size_n; i_++) {
                     if (i_ != i) {
                         sum_on_max += std::max(0.0, matrix_R[i_][k]);
                     }
@@ -145,12 +146,11 @@ std::vector<std::vector<double> > calculate_affinity_propagation(const std::vect
         // Calculate C matrix
         changed = false;
 #pragma omp parallel for collapse(2) default(none) shared(matrix_A, matrix_R, matrix_C, size_n, changed)
-        for (int i = 0; i < size_n; i++) {
-            for (int k = 0; k < size_n; k++) {
+        for (size_t i = 0; i < size_n; i++) {
+            for (size_t k = 0; k < size_n; k++) {
                 const double new_value = matrix_A[i][k] + matrix_R[i][k];
                 const double old_value = matrix_C[i][k];
                 changed = changed || (new_value != old_value);
-                std::cout << "Changed: " << changed << std::endl;
                 matrix_C[i][k] = new_value;
             }
         }
@@ -165,11 +165,8 @@ std::vector<std::vector<double> > calculate_affinity_propagation(const std::vect
 }
 
 void create_clusters(const std::vector<std::vector<double> > &matrix_C) {
-    // Find maximum value in each row of C
-    print_matrix(matrix_C, "Criterion matrix");
-
     auto max_values = std::vector<double>(matrix_C.size());
-    auto max_indexes = std::vector<int64_t>(matrix_C.size()); // Changed from long to int64_t
+    auto max_indexes = std::vector<int64_t>(matrix_C.size());
     for (size_t i = 0; i < matrix_C.size(); i++) {
         auto a = std::ranges::max_element(matrix_C[i]);
         max_values[i] = *a;
@@ -182,8 +179,7 @@ void create_clusters(const std::vector<std::vector<double> > &matrix_C) {
 }
 
 int main() {
-    std::cout << "OpenMP version: " << _OPENMP << std::endl;
-
+    int max_iteration = 1000;
 
     // five participants
     const std::string five_participant_file = "../project_2/five_participants.csv";
@@ -191,7 +187,10 @@ int main() {
     const std::vector<std::vector<double> > five_participant_matrix = tokenize_csv(five_participant_dataset);
     const std::vector<std::vector<double> > five_participants_similarity_matrix = calculate_similarity_matrix(
         five_participant_matrix);
-    create_clusters(five_participants_similarity_matrix);
+    const std::vector<std::vector<double> > five_participants_clusters = calculate_affinity_propagation(
+        five_participants_similarity_matrix, max_iteration);
+    create_clusters(five_participants_clusters);
+
 
     // Iris dataset
     // const std::string iris_file = "../project_2/iris.csv";
