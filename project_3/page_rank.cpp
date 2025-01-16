e980cc66bd3f6f202fafa08a368f522f8b24e0dd
Vojtech Sliva <sli0124@vsb.cz>
2025-01-16 17:53:07 +0100

    feat(proj3): loading data into structure

--- project_3/page_rank.cpp
+++ project_3/page_rank.cpp
@@ -3,6 +3,14 @@
 #include <thread>
 #include <vector>
 #include <string>
+#include <unordered_map>
+#include <sstream>
+#include <chrono>
+
+struct Vertex {
+    std::unordered_map<int, std::vector<int> > n_minus;
+    std::unordered_map<int, std::vector<int> > n_plus;
+};
 
 std::string trim_line(const std::string &str) {
     size_t start = 0;
@@ -21,7 +29,7 @@ std::string trim_line(const std::string &str) {
 void load_data_worker(const std::string &filename,
                       const size_t start_line,
                       const size_t end_line,
-                      std::vector<std::string> &local_result) {
+                      std::vector<Vertex> &local_vertices) {
     std::ifstream file(filename);
     if (!file.is_open()) {
         std::cerr << "Error opening file: " << filename << std::endl;
@@ -29,24 +37,31 @@ void load_data_worker(const std::string &filename,
     }
 
     std::string line;
-    size_t current_line = 0;
-
-    while (current_line < start_line && std::getline(file, line)) {
-        current_line++;
-    }
+    size_t current_line = start_line;
 
     while (current_line < end_line && std::getline(file, line)) {
         line = trim_line(line);
-        if (!line.empty()) {
-            local_result.push_back(line);
+        if (line.empty() || line[0] == '#') {
+            current_line++;
+            continue;
         }
+
+        std::istringstream iss(line);
+        int from, to;
+        iss >> from >> to;
+
+        Vertex vertex;
+        vertex.n_minus[from].push_back(to);
+        vertex.n_plus[to].push_back(from);
+        local_vertices.push_back(vertex);
+
         current_line++;
     }
 
     file.close();
 }
 
-std::vector<std::string> load_data(const std::string &filename) {
+std::vector<Vertex> load_data(const std::string &filename) {
     std::ifstream file(filename);
     if (!file.is_open()) {
         std::cerr << "Error opening file: " << filename << std::endl;
@@ -63,16 +78,11 @@ std::vector<std::string> load_data(const std::string &filename) {
     const size_t threads_count = std::thread::hardware_concurrency();
     const size_t chunk_size = total_lines / threads_count;
     std::vector<std::thread> threads;
-    std::vector<std::vector<std::string> > partial_results(threads_count);
+    std::vector<std::vector<Vertex> > partial_results(threads_count);
 
     for (size_t i = 0; i < threads_count; ++i) {
         size_t start_line = i * chunk_size;
-        size_t end_line;
-        if (i == threads_count - 1) {
-            end_line = total_lines;
-        } else {
-            end_line = (i + 1) * chunk_size;
-        }
+        size_t end_line = (i == threads_count - 1) ? total_lines : (i + 1) * chunk_size;
 
         threads.emplace_back(load_data_worker, filename, start_line, end_line, std::ref(partial_results[i]));
     }
@@ -81,7 +91,7 @@ std::vector<std::string> load_data(const std::string &filename) {
         thread.join();
     }
 
-    std::vector<std::string> final_result;
+    std::vector<Vertex> final_result;
     for (const auto &partial_result: partial_results) {
         final_result.insert(final_result.end(), partial_result.begin(), partial_result.end());
     }
@@ -93,12 +103,12 @@ int main() {
     const std::string filename = "../project_3/web-BerkStan.txt";
 
     const std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    const std::vector<std::string> data = load_data(filename);
+    const std::vector<Vertex> all_vertices = load_data(filename);
     const std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
     std::cout << "Time for loading data: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count()
             << "ms" << std::endl;
-    std::cout << "Loaded " << data.size() << " lines of data." << std::endl;
+    std::cout << "Loaded " << all_vertices.size() << " vertices." << std::endl;
 
     return 0;
 }
