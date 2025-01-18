06928ecdddc8f3b042337f7f329c7b6b55178528
Vojtech Sliva <sli0124@vsb.cz>
2025-01-18 13:50:14 +0100

    fix(proj3): better structure and loading and for data

--- project_3/page_rank.cpp
+++ project_3/page_rank.cpp
@@ -7,108 +7,121 @@
 #include <sstream>
 #include <chrono>
 
-struct Vertex {
+struct AdjacencyList {
     std::unordered_map<int, std::vector<int> > n_minus;
     std::unordered_map<int, std::vector<int> > n_plus;
 };
 
-std::string trim_line(const std::string &str) {
-    size_t start = 0;
-    while (start < str.size() && isspace(str[start])) {
-        start++;
-    }
-
-    size_t end = str.size();
-    while (end > start && isspace(str[end - 1])) {
-        end--;
-    }
-
-    return str.substr(start, end - start);
-}
-
 void load_data_worker(const std::string &filename,
-                      const size_t start_line,
-                      const size_t end_line,
-                      std::vector<Vertex> &local_vertices) {
+                      const std::streampos start_pos,
+                      const std::streampos end_pos,
+                      AdjacencyList &local_adjacency_list) {
     std::ifstream file(filename);
     if (!file.is_open()) {
         std::cerr << "Error opening file: " << filename << std::endl;
         return;
     }
 
+    file.seekg(start_pos);
     std::string line;
-    size_t current_line = start_line;
-
-    while (current_line < end_line && std::getline(file, line)) {
-        line = trim_line(line);
+    while (file.tellg() < end_pos && std::getline(file, line)) {
         if (line.empty() || line[0] == '#') {
-            current_line++;
             continue;
         }
 
+        int source, target;
         std::istringstream iss(line);
-        int from, to;
-        iss >> from >> to;
+        iss >> source >> target;
 
-        Vertex vertex;
-        vertex.n_minus[from].push_back(to);
-        vertex.n_plus[to].push_back(from);
-        local_vertices.push_back(vertex);
-
-        current_line++;
+        local_adjacency_list.n_minus[source].push_back(target);
+        local_adjacency_list.n_plus[target].push_back(source);
     }
 
     file.close();
 }
 
-std::vector<Vertex> load_data(const std::string &filename) {
-    std::ifstream file(filename);
+AdjacencyList load_data(const std::string &filename) {
+    std::ifstream file(filename, std::ios::ate | std::ios::binary);
     if (!file.is_open()) {
         std::cerr << "Error opening file: " << filename << std::endl;
         return {};
     }
 
-    size_t total_lines = 0;
-    std::string line;
-    while (std::getline(file, line)) {
-        total_lines++;
+    std::streampos file_size = file.tellg();
+    file.seekg(0);
+
+    const long num_threads = std::thread::hardware_concurrency();
+    const long chunk_size = file_size / num_threads;
+    std::vector<std::streampos> chunk_boundaries;
+    chunk_boundaries.reserve(num_threads + 1);
+    chunk_boundaries.emplace_back(0);
+
+    for (int i = 1; i < num_threads; ++i) {
+        file.seekg(i * chunk_size);
+
+        std::string dummy;
+        std::getline(file, dummy);
+
+        chunk_boundaries.push_back(file.tellg());
     }
+
+    chunk_boundaries.push_back(file_size);
     file.close();
 
-    const size_t threads_count = std::thread::hardware_concurrency();
-    const size_t chunk_size = total_lines / threads_count;
     std::vector<std::thread> threads;
-    std::vector<std::vector<Vertex> > partial_results(threads_count);
+    std::vector<AdjacencyList> partial_results(num_threads);
 
-    for (size_t i = 0; i < threads_count; ++i) {
-        size_t start_line = i * chunk_size;
-        size_t end_line = (i == threads_count - 1) ? total_lines : (i + 1) * chunk_size;
-
-        threads.emplace_back(load_data_worker, filename, start_line, end_line, std::ref(partial_results[i]));
+    for (int i = 0; i < num_threads; ++i) {
+        threads.emplace_back(load_data_worker,
+                             filename,
+                             chunk_boundaries[i],
+                             chunk_boundaries[i + 1],
+                             std::ref(partial_results[i]));
     }
 
     for (auto &thread: threads) {
         thread.join();
     }
 
-    std::vector<Vertex> final_result;
-    for (const auto &partial_result: partial_results) {
-        final_result.insert(final_result.end(), partial_result.begin(), partial_result.end());
+    AdjacencyList final_result;
+    for (const auto &[n_minus, n_plus]: partial_results) {
+        for (const auto &[key, values]: n_minus) {
+            final_result.n_minus[key].insert(final_result.n_minus[key].end(), values.begin(), values.end());
+        }
+        for (const auto &[key, values]: n_plus) {
+            final_result.n_plus[key].insert(final_result.n_plus[key].end(), values.begin(), values.end());
+        }
     }
 
     return final_result;
 }
 
+
+size_t get_total_node_count(const AdjacencyList &all_vertices) {
+    std::unordered_map<int, std::vector<int> > all_vertices_map;
+
+    for (const auto &[key, values]: all_vertices.n_minus) {
+        all_vertices_map[key].insert(all_vertices_map[key].end(), values.begin(), values.end());
+    }
+    for (const auto &[key, values]: all_vertices.n_plus) {
+        all_vertices_map[key].insert(all_vertices_map[key].end(), values.begin(), values.end());
+    }
+
+    return all_vertices_map.size();
+}
+
+
 int main() {
     const std::string filename = "../project_3/web-BerkStan.txt";
 
     const std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    const std::vector<Vertex> all_vertices = load_data(filename);
+    const AdjacencyList all_vertices = load_data(filename);
     const std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
     std::cout << "Time for loading data: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count()
             << "ms" << std::endl;
-    std::cout << "Loaded " << all_vertices.size() << " vertices." << std::endl;
+    const size_t total_node_count = get_total_node_count(all_vertices);
+    std::cout << "Total number of nodes: " << total_node_count << std::endl;
 
     return 0;
 }
